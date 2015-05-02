6a0051fb3dc54066696fac4ef6ed35135eb45f50
happynear <wf900911@163.com>
2015-05-02 15:58:27 +0800

    chg

--- src/caffe/proto/extract_proto.bat
+++ src/caffe/proto/extract_proto.bat
@@ -1,2 +1,6 @@
 protoc caffe.proto --cpp_out=./
+protoc caffe.proto --python_out=./
+md ..\..\..\python\caffe\proto\
+copy /y .\caffe_pb2.py ..\..\..\python\caffe\proto\
+copy nul ..\..\..\python\caffe\proto\__init__.py
 pause
