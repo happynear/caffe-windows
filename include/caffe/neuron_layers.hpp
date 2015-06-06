9d9e87046287b53f86ce0921be6e256a541fe1f1
happynear <wf900911@163.com>
2015-06-06 23:53:28 +0800

    remove insanity layer. weird bug.

--- include/caffe/neuron_layers.hpp
+++ include/caffe/neuron_layers.hpp
@@ -720,57 +720,6 @@ class ThresholdLayer : public NeuronLayer<Dtype> {
 };
 
 /**
- * @brief Randomized Leaky Rectified Linear Unit @f$
- *        y_i = \max(0, x_i) + frac{\min(0, x_i)}{a_i}
- *        @f$. The negative slope is randomly generated from
- *        uniform distribution U(lb, ub). 
- */
-template <typename Dtype>
-class InsanityLayer : public NeuronLayer<Dtype> {
- public:
-  /**
-   * @param param provides InsanityParameter insanity_param,
-   */
-  explicit InsanityLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual inline const char* type() const { return "Insanity"; }
-
- protected:
-  /**
-   * @param bottom input Blob vector (length 1)
-   *   -# @f$ (N \times C \times ...) @f$
-   *      the inputs @f$ x @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (N \times C \times ...) @f$
-   *      the computed outputs for each channel @f$i@f$ @f$
-   *        y_i = \max(0, x_i) + a_i \min(0, x_i)
-   *      @f$.
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-
-  Dtype lb_, ub_, mean_slope;
-  int saturation_start_, saturation_end_;
-  Blob<Dtype> alpha;  // random generated negative slope
-  Blob<Dtype> bottom_memory_;  // memory for in-place computation
-};
-
-/**
  * @brief Parameterized Rectified Linear Unit non-linearity @f$
  *        y_i = \max(0, x_i) + a_i \min(0, x_i)
  *        @f$. The differences from ReLULayer are 1) negative slopes are
