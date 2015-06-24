f8d841eb6a50930a20fe279b2bfad45cf732d31c
happynear <wf900911@163.com>
2015-06-24 14:59:01 +0800

    chg

--- matlab/demo/Inceptionism/Inceptionism.m
+++ matlab/demo/Inceptionism/Inceptionism.m
@@ -28,12 +28,21 @@ mean_image = caffe.read_mean(mean_file);
 mean_image = mean_image(17:240,17:240,:);
 % mean_image = mean_image + randn(size(mean_image));
 input_data = zeros(size(mean_image,1), size(mean_image,2), 3, 1, 'single');
+% input_data(120,120,:) = 1;
+% input_data(60:180,60,:) =1;
+% input_data(60,60:180,:) = 1;
+% input_data(180,60:180,:) = 1;
+% input_data(60:180,180,:) = 1;
 
 % trans_image = single(imread('e:\\banana.png'));
 trans_image = imresize(trans_image,[size(mean_image,1), size(mean_image,2)]);
 % input_data(:,:,:,1) = trans_image - mean_image;%randn(size(mean_image));
-input_data(:,:,:,1) = randn(size(mean_image)) / 5;
+input_data(:,:,:,1) = randn(size(mean_image)) * 10;
 % input_data(:,:,:,1) = zeros(size(mean_image));
+% H = fspecial('gaussian',[20 20],10);
+% input_data(:,:,:,1) = imfilter(input_data(:,:,:,1),H,'same');
+% input_data = input_data / std(input_data(:)) * 30;
+
 
 % output_blob_index = train_net.name2blob_index('pool3');%feature blob
 % output_blob = train_net.blob_vec(output_blob_index);
@@ -41,18 +50,20 @@ input_data(:,:,:,1) = randn(size(mean_image)) / 5;
 % output_label = train_net.blob_vec(output_label_index);
 prob = train_net.forward({input_data});
 [max_prob,max_idx] = max(prob{1});
-max_idx = 328;
+max_idx = 784;
 this_prob = prob{1}(max_idx);
 back_data = zeros(size(prob{1}),'single');
 back_data(max_idx) = 1;
-base_lr = 30;
-lambda1 = 0.003;
-lambda2 = 0.002;
+base_lr = 10;
+lambda1 = 0.0001;
+lambda2 = 0.0002;
 last_prob = -999;
 momentum = 0.8;
 lastgrad = zeros(size(mean_image));
-H = fspecial('gaussian',[7 7],5);
-while this_prob<0.9999999999
+H = fspecial('gaussian',[7 7],0.8);
+iter = 1;
+dropout = 0.5;
+while 1
 %     lr = base_lr;
     lr = base_lr;% * sqrt(this_prob / (1 - this_prob));
 %     if this_prob>0.9
@@ -77,37 +88,72 @@ while this_prob<0.9999999999
     
     bak_data = input_data;
     
-    res{1} = imfilter(res{1},H,'same');
+%     res{1} = imfilter(res{1},H,'same');
     
-    lastgrad = (1 - momentum) * lr * res{1} / norm(res{1}(:)) + momentum * lastgrad;
+    lastgrad = (1 - momentum) * lr * res{1} / norm(res{1}(:))  + momentum * lastgrad;%
     input_data(:,:,:,1) = input_data(:,:,:,1) + lastgrad;
     
 %     if this_prob > 0.1
         I = input_data(:,:,:,1);
-        Gx = sign(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - sign(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
-        Gy = sign(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - sign(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
+%         Gx = sign(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - sign(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
+%         Gy = sign(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - sign(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
 %         Gx = smoothL1(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - smoothL1(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
 %         Gy = smoothL1(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - smoothL1(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
-%         Gx = (I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - (I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
-%         Gy = (I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - (I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
-        input_data(2:end-1,2:end-1,:,1) = input_data(2:end-1,2:end-1,:,1) - lr * lambda2 * (Gx + Gy);
-        input_data(:,:,:,1) = input_data(:,:,:,1) + lr * lambda1 * I;
+
+        Gx = sign(I(2:end-1,:,:) - I(1:end-2,:,:)) - sign(I(3:end,:,:) - I(2:end-1,:,:));
+        Gx = [sign(I(1,:,:) - I(2,:,:)); Gx; sign(I(end,:,:) - I(end-1,:,:))];
+        Gy = sign(I(:,2:end-1,:) - I(:,1:end-2,:)) - sign(I(:,3:end,:) - I(:,2:end-1,:));
+        Gy = [sign(I(:,1,:) - I(:,2,:)) Gy sign(I(:,end,:) - I(:,end-1,:))];
+        input_data(:,:,:,1) = input_data(:,:,:,1) - lr * lambda2 * (Gx + Gy);
+%         input_data(:,:,:,1) = input_data(:,:,:,1) - lr * lambda1 * (I);
+%         input_data = (input_data -mean(input_data(:))) / std(input_data(:)) * 30;
 %     end;
+
+    for_forward = reshape(input_data,[size(mean_image,1)*size(mean_image,2) 3]);
+    mask = rand(size(mean_image,1), size(mean_image,2)) < dropout;
+    for_forward(mask==1,:) = 0;
+    for_forward = reshape(for_forward,size(input_data));
     prob = train_net.forward({input_data});
     this_prob = prob{1}(max_idx);
-    fprintf('lr=%f,prob=%f,last_prob=%f\n',lr,this_prob,last_prob);
-    if this_prob<last_prob
-        base_lr = base_lr * 0.9;
-        input_data = bak_data;
+    fprintf('iter=%d,lr=%f,prob=%f,last_prob=%f\n',iter,lr,this_prob,last_prob);
+    iter = iter + 1;
+    if mod(iter,10) ==0%&&this_prob>0.7
+%         if iter<700
+            input_data(:,:,:,1) = imfilter(input_data(:,:,:,1),H,'same');
+%         else
+%             input_data(:,:,:,1) = imguidedfilter(input_data(:,:,:,1));
+%         end;
     end;
-    if this_prob>last_prob%&& (this_prob-last_prob) / this_prob < 0.001
-        base_lr = base_lr * 1.1;
+    if mod(iter,100)==0
+        figure(1)
+        output = reshape(input_data,[size(input_data,1)*size(input_data,2) size(input_data,3)]);
+        output = zscore(output);
+        output = reshape(output,[size(input_data,1),size(input_data,2),size(input_data,3)]);
+        output = output(:, :, [3, 2, 1]);
+        imshow(uint8(output*255));
+        figure(2);
+        % imshow(uint8(mean_image + input_data));
+        output = mean_image + input_data(:,:,:,1);
+        output = output(:, :, [3, 2, 1]);
+        imshow(uint8(output));
+        I = output;
+        Gx = abs(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) + abs(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
+        Gy = abs(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) + abs(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
+        figure(3);hist(Gx(:),1000);
+        figure(4);hist(Gy(:),1000);
+    end;
+    if this_prob<last_prob
+        base_lr = base_lr * 0.99;
 %         input_data = bak_data;
     end;
-    if this_prob>last_prob
-        last_prob = this_prob;
+    if this_prob>last_prob&&base_lr<2000%&& (this_prob-last_prob) / this_prob < 0.001
+        base_lr = base_lr * 1.01;
+%         input_data = bak_data;
     end;
-    if lr<0.0001
+%     if this_prob>last_prob
+        last_prob = this_prob;
+%     end;
+    if lr<0.000001
         break;
     end;
 end;
