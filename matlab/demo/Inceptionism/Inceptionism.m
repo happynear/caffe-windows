c6792a1fde1ae8ab0fb6f0d28937d66aab6ff2f5
happynear <wf900911@163.com>
2015-06-25 11:27:17 +0800

    inceptionism

--- matlab/demo/Inceptionism/Inceptionism.m
+++ matlab/demo/Inceptionism/Inceptionism.m
@@ -40,19 +40,20 @@ input_data = zeros(size(mean_image,1), size(mean_image,2), 3, 1, 'single');
 % trans_image = single(imread('e:\\banana.png'));
 % trans_image = imresize(trans_image,[size(mean_image,1), size(mean_image,2)]);
 % input_data(:,:,:,1) = trans_image - mean_image;%randn(size(mean_image));
-input_data(:,:,:,1) = randn(size(mean_image)) * 10;
+input_data(:,:,:,1) = randn(size(mean_image)) * 50;
 % input_data(:,:,:,1) = zeros(size(mean_image));
 % H = fspecial('gaussian',[20 20],10);
 % input_data(:,:,:,1) = imfilter(input_data(:,:,:,1),H,'same');
 % input_data = input_data / std(input_data(:)) * 30;
 
-use_clip = true;
+use_clip = false;
 use_cv_norm = true;
 use_weight_decay = false;
-use_image_blur = false;
+use_image_blur = true;
 use_gradient_blur = false;
+use_dropout = false;
 
-H = fspecial('gaussian',[7 7],1);
+H = fspecial('gaussian',[7 7],1.2);
 % nth_layer = train_net.layer_vec(train_net.name2layer_index('blur'));
 % blur_kernel = zeros(7,7,3,3);
 % blur_kernel(:,:,1,1) = H;
@@ -62,18 +63,19 @@ H = fspecial('gaussian',[7 7],1);
 
 prob = train_net.forward({input_data});
 [max_prob,max_idx] = max(prob{3});
-max_idx = 955;
+max_idx = 473;
 this_prob = prob{3}(max_idx);
-back_data = zeros(size(prob{3}),'single');
+back_data = ones(size(prob{3}),'single') * -1;
 back_data(max_idx) = 1;
 back_cell = prob;
 back_cell{1} = back_data;
 back_cell{2} = back_data;
 back_cell{3} = back_data;
 blur_data = zeros(size(input_data));
-base_lr = 100;
-lambda1 = 0.0001;
-lambda2 = 0.0004;
+base_lr = 1;
+max_lr = 2000;
+lambda1 = 0.00001;
+lambda2 = 0.0001;
 last_prob = -999;
 momentum = 0.8;
 lastgrad = zeros(size(mean_image));
@@ -101,7 +103,7 @@ while 1
     end;
     
     
-    lastgrad = (1 - momentum) * lr * res{1} / norm(res{1}(:))  + momentum * lastgrad;%
+    lastgrad = (1 - momentum) * lr * res{1}   + momentum * lastgrad;%/ norm(res{1}(:))
 %     lastgrad = reshape(lastgrad,[size(mean_image,1)*size(mean_image,2) 3]);
 %     lastgrad(mask==1,:) = 0;
 %     lastgrad = reshape(lastgrad,size(input_data));
@@ -111,12 +113,14 @@ while 1
         I = input_data(:,:,:,1);
 %         Gx = sign(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - sign(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
 %         Gy = sign(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - sign(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
-%         Gx = smoothL1(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) - smoothL1(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
-%         Gy = smoothL1(I(2:end-1,2:end-1,:) - I(2:end-1,1:end-2,:)) - smoothL1(I(2:end-1,3:end,:) - I(2:end-1,2:end-1,:));
-        Gx = sign(I(2:end-1,:,:) - I(1:end-2,:,:)) - sign(I(3:end,:,:) - I(2:end-1,:,:));
-        Gx = [sign(I(1,:,:) - I(2,:,:)); Gx; sign(I(end,:,:) - I(end-1,:,:))];
-        Gy = sign(I(:,2:end-1,:) - I(:,1:end-2,:)) - sign(I(:,3:end,:) - I(:,2:end-1,:));
-        Gy = [sign(I(:,1,:) - I(:,2,:)) Gy sign(I(:,end,:) - I(:,end-1,:))];
+        Gx = smoothL1(I(2:end-1,:,:) - I(1:end-2,:,:)) - smoothL1(I(3:end,:,:) - I(2:end-1,:,:));
+        Gx = [smoothL1(I(1,:,:) - I(2,:,:)); Gx; smoothL1(I(end,:,:) - I(end-1,:,:))];
+        Gy = smoothL1(I(:,2:end-1,:) - I(:,1:end-2,:)) - smoothL1(I(:,3:end,:) - I(:,2:end-1,:));
+        Gy = [smoothL1(I(:,1,:) - I(:,2,:)) Gy smoothL1(I(:,end,:) - I(:,end-1,:))];
+%         Gx = sign(I(2:end-1,:,:) - I(1:end-2,:,:)) - sign(I(3:end,:,:) - I(2:end-1,:,:));
+%         Gx = [sign(I(1,:,:) - I(2,:,:)); Gx; sign(I(end,:,:) - I(end-1,:,:))];
+%         Gy = sign(I(:,2:end-1,:) - I(:,1:end-2,:)) - sign(I(:,3:end,:) - I(:,2:end-1,:));
+%         Gy = [sign(I(:,1,:) - I(:,2,:)) Gy sign(I(:,end,:) - I(:,end-1,:))];
         input_data(:,:,:,1) = input_data(:,:,:,1) - lr * lambda2 * (Gx + Gy);
     end;
     if use_weight_decay
@@ -130,16 +134,15 @@ while 1
 %     for_forward(mask==1,:) = 0;
 %     for_forward = reshape(for_forward,size(input_data));
     
-    if mod(iter,10) ==0&&use_image_blur&&iter<3000
-        blur_data = input_data;
-        blur_data(:,:,:,1) = imfilter(blur_data(:,:,:,1),H,'same');
-        prob = train_net.forward({blur_data});
-    else
-        prob = train_net.forward({input_data});
+    if mod(iter,10) ==0&&use_image_blur&&iter<2000
+%         blur_data = input_data;
+%         blur_data(:,:,:,1) = imfilter(blur_data(:,:,:,1),H,'same');
+        input_data(:,:,:,1) = imfilter(input_data(:,:,:,1),H,'same');
     end;
+    prob = train_net.forward({input_data});
     
     this_prob = prob{3}(max_idx);
-    fprintf('iter=%d,lr=%f,prob=%f,last_prob=%f\n',iter,lr,this_prob,last_prob);
+    fprintf('iter=%d,lr=%f,prob1=%f,prob2=%f,prob3=%f,last_prob=%f\n',iter,lr,prob{1}(max_idx),prob{2}(max_idx),this_prob,last_prob);
     iter = iter + 1;
     
     if mod(iter,100)==0
@@ -148,11 +151,13 @@ while 1
         output = zscore(output);
         output = reshape(output,[size(input_data,1),size(input_data,2),size(input_data,3)]);
         output = output(:, :, [3, 2, 1]);
+        output = permute(output, [2 1 3]);
         imshow(uint8(output*255));
         figure(2);
         % imshow(uint8(mean_image + input_data));
         output = mean_image + input_data(:,:,:,1);
         output = output(:, :, [3, 2, 1]);
+        output = permute(output, [2 1 3]);
         imshow(uint8(output));
         I = output;
         Gx = abs(I(2:end-1,2:end-1,:) - I(1:end-2,2:end-1,:)) + abs(I(3:end,2:end-1,:) - I(2:end-1,2:end-1,:));
@@ -164,7 +169,7 @@ while 1
         base_lr = base_lr * 0.99;
 %         input_data = bak_data;
     end;
-    if this_prob>last_prob&&base_lr<2000%&& (this_prob-last_prob) / this_prob < 0.001
+    if this_prob>last_prob&&base_lr<max_lr%&& (this_prob-last_prob) / this_prob < 0.001
         base_lr = base_lr * 1.01;
 %         input_data = bak_data;
     end;
