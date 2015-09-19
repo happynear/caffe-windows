89a4d7ca4a2bd9b8647cfa68b531ed1512962611
happynear <wf900911@163.com>
2015-09-20 01:18:01 +0800

    covariance layer for NeuralArt

--- include/caffe/custom_layers.hpp
+++ include/caffe/custom_layers.hpp
@@ -371,6 +371,34 @@ namespace caffe {
 		Blob<Dtype> CoordinateSource;//2*(nwh)
 		Blob<Dtype> InterpolateWeight;//4*(nwh)
 	};
+	
+	/**
+	* @brief Compute covariance matrix for the feature.
+	*
+	* TODO(dox): thorough documentation for Forward, Backward, and proto params.
+	*/
+	template <typename Dtype>
+	class CovarianceLayer : public Layer<Dtype> {
+	public:
+		explicit CovarianceLayer(const LayerParameter& param)
+			: Layer<Dtype>(param) {}
+		virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+			const vector<Blob<Dtype>*>& top);
+
+		virtual inline const char* type() const { return "Covariance"; }
+		virtual inline int ExactNumBottomBlobs() const { return 1; }
+		virtual inline int MinTopBlobs() const { return 1; }
+
+	protected:
+		virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+			const vector<Blob<Dtype>*>& top);
+		virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+			const vector<Blob<Dtype>*>& top);
+		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+			const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+		virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+			const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+	};
 
 }  // namespace caffe
 
