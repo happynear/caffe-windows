5746a815917c410e96d67b0ba1e2b463af2786f9
pan <youkpan@gmail.com>
2016-03-09 10:23:20 +0800

    add summar data import

--- examples/mnist/convert_mnist_data.cpp
+++ examples/mnist/convert_mnist_data.cpp
@@ -40,32 +40,52 @@ uint32_t swap_endian(uint32_t val) {
 void convert_dataset(const char* image_filename, const char* label_filename,
 	const char* db_path, const string& db_backend) {
 	// Open files
+
+	bool usesummary = atoi(label_filename) > 1;// strcmp(label_filename, "summary") == 0;
 	std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
-	std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+	std::ifstream label_file;
+	if (!usesummary){
+		std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+		CHECK(label_file) << "Unable to open file " << label_filename;
+	}
+
+
 	CHECK(image_file) << "Unable to open file " << image_filename;
-	CHECK(label_file) << "Unable to open file " << label_filename;
+	
 	// Read the magic and the meta data
 	uint32_t magic;
-	uint32_t num_items;
+	uint64_t num_items;
 	uint32_t num_labels;
 	uint32_t rows;
 	uint32_t cols;
 
-	image_file.read(reinterpret_cast<char*>(&magic), 4);
-	magic = swap_endian(magic);
-	CHECK_EQ(magic, 2051) << "Incorrect image file magic.";
-	label_file.read(reinterpret_cast<char*>(&magic), 4);
-	magic = swap_endian(magic);
-	CHECK_EQ(magic, 2049) << "Incorrect label file magic.";
-	image_file.read(reinterpret_cast<char*>(&num_items), 4);
-	num_items = swap_endian(num_items);
-	label_file.read(reinterpret_cast<char*>(&num_labels), 4);
-	num_labels = swap_endian(num_labels);
-	CHECK_EQ(num_items, num_labels);
-	image_file.read(reinterpret_cast<char*>(&rows), 4);
-	rows = swap_endian(rows);
-	image_file.read(reinterpret_cast<char*>(&cols), 4);
-	cols = swap_endian(cols);
+	if (!usesummary)
+	{
+		image_file.read(reinterpret_cast<char*>(&magic), 4);
+		magic = swap_endian(magic);
+		CHECK_EQ(magic, 2051) << "Incorrect image file magic.";
+		label_file.read(reinterpret_cast<char*>(&magic), 4);
+		magic = swap_endian(magic);
+		CHECK_EQ(magic, 2049) << "Incorrect label file magic.";
+		image_file.read(reinterpret_cast<char*>(&num_items), 4);
+		num_items = swap_endian(num_items);
+		label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+		num_labels = swap_endian(num_labels);
+		CHECK_EQ(num_items, num_labels);
+		image_file.read(reinterpret_cast<char*>(&rows), 4);
+		rows = swap_endian(rows);
+		image_file.read(reinterpret_cast<char*>(&cols), 4);
+		cols = swap_endian(cols);
+	}
+	else
+	{
+		num_items = atoi(label_filename);
+		rows = 22;
+		cols = 22;
+	}
+
+//title:
+
 
 	// lmdb
 	MDB_env *mdb_env;
@@ -114,6 +134,9 @@ void convert_dataset(const char* image_filename, const char* label_filename,
 	const int kMaxKeyLength = 10;
 	char key_cstr[kMaxKeyLength];
 	string value;
+	int mpos=0;
+	int mpos2=0;
+	int label_i = 0;
 
 	Datum datum;
 	datum.set_channels(1);
@@ -121,9 +144,47 @@ void convert_dataset(const char* image_filename, const char* label_filename,
 	datum.set_width(cols);
 	LOG(INFO) << "A total of " << num_items << " items.";
 	LOG(INFO) << "Rows: " << rows << " Cols: " << cols;
+	string sline;
 	for (int item_id = 0; item_id < num_items; ++item_id) {
-		image_file.read(pixels, rows * cols);
-		label_file.read(&label, 1);
+
+		if (usesummary){
+			getline(image_file, sline, '\n');
+			if (sline.length() < 2)
+				continue;
+			//3:1.92:
+			mpos = sline.find_first_of(':');
+			mpos2 = mpos;
+			if (mpos > 10 || mpos <= 0)
+				continue;
+
+			if (strcmp(sline.substr(0, 5).c_str(), "title") != 0){
+				label_i = stoi(sline.substr(0, mpos));
+				if (sline.find_first_of(':', mpos + 1) < 10); {
+					mpos2 = sline.find_first_of(':', mpos + 1);
+				}
+			}
+			else
+				label_i = 5;
+
+			mpos = sline.substr(mpos2 + 1).length();
+			if (mpos > rows*cols - 1)
+				mpos = rows*cols - 1;
+			else if (mpos < 10)
+			{
+				if (num_items == 99999)//99999 just for testing
+					LOG(INFO) << " item_id:" << item_id <<" label:" << label_i << " str:" << sline.substr(mpos2 + 1) << " mpos2:" << mpos2;
+				continue;
+			}
+
+			memset(pixels, 0, rows*cols);
+			memcpy(pixels, sline.substr(mpos2 + 1).c_str(),  mpos);
+			label =(char) label_i ;
+
+		}
+		else{
+			image_file.read(pixels, rows * cols);
+			label_file.read(&label, 1);
+		}
 		datum.set_data(pixels, rows*cols);
 		datum.set_label(label);
 		sprintf_s(key_cstr, kMaxKeyLength, "%08d", item_id);
