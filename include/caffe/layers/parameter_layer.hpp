8d87f97fcf5010623fbba8353e26513e539a62f1
happynear <wf900911@163.com>
2017-01-19 11:47:56 -0500

    add gpu forward/backward for parameter layer.

--- include/caffe/layers/parameter_layer.hpp
+++ include/caffe/layers/parameter_layer.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "caffe/layer.hpp"
+#include "caffe/filler.hpp"
 
 namespace caffe {
 
@@ -13,10 +14,11 @@ class ParameterLayer : public Layer<Dtype> {
   explicit ParameterLayer(const LayerParameter& param)
       : Layer<Dtype>(param) {}
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
+                          const vector<Blob<Dtype>*>& top) {
     if (this->blobs_.size() > 0) {
       LOG(INFO) << "Skipping parameter initialization";
-    } else {
+    }
+    else {
       this->blobs_.resize(1);
       this->blobs_[0].reset(new Blob<Dtype>());
       this->blobs_[0]->Reshape(this->layer_param_.parameter_param().shape());
@@ -26,20 +28,34 @@ class ParameterLayer : public Layer<Dtype> {
     top[0]->Reshape(this->layer_param_.parameter_param().shape());
   }
   virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) { }
+                       const vector<Blob<Dtype>*>& top) {
+  }
   virtual inline const char* type() const { return "Parameter"; }
   virtual inline int ExactNumBottomBlobs() const { return 0; }
   virtual inline int ExactNumTopBlobs() const { return 1; }
 
  protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-    top[0]->ShareData(*(this->blobs_[0]));
-    top[0]->ShareDiff(*(this->blobs_[0]));
-  }
+   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+                            const vector<Blob<Dtype>*>& top) {
+     top[0]->ShareData(*(this->blobs_[0]));
+   }
   virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom)
-  { }
+                            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    if (this->param_propagate_down_[0]) {
+      this->blobs_[0]->ShareDiff(*top[0]);
+    }
+  }
+
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+                           const vector<Blob<Dtype>*>& top) {
+    top[0]->ShareData(*(this->blobs_[0]));
+  }
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+                            const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    if (this->param_propagate_down_[0]) {
+      this->blobs_[0]->ShareDiff(*top[0]);
+    }
+  }
 };
 
 }  // namespace caffe
