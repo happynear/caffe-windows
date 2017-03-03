beaef2c5cf5615edc8e07266a9632179d508acef
happynear <wf900911@163.com>
2017-03-02 19:30:19 -0500

    copy param even if shape dismatch

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -753,18 +753,22 @@ void Net<Dtype>::CopyTrainedLayersFrom(const NetParameter& param) {
         << "Incompatible number of blobs for layer " << source_layer_name;
     for (int j = 0; j < target_blobs.size(); ++j) {
       if (!target_blobs[j]->ShapeEquals(source_layer.blobs(j))) {
-        Blob<Dtype> source_blob;
         const bool kReshape = true;
-        source_blob.FromProto(source_layer.blobs(j), kReshape);
-        LOG(FATAL) << "Cannot copy param " << j << " weights from layer '"
-            << source_layer_name << "'; shape mismatch.  Source param shape is "
-            << source_blob.shape_string() << "; target param shape is "
-            << target_blobs[j]->shape_string() << ". "
-            << "To learn this layer's parameters from scratch rather than "
-            << "copying from a saved net, rename the layer.";
+        target_blobs[j]->FromProto(source_layer.blobs(j), kReshape);
+        //Blob<Dtype> source_blob;
+        //const bool kReshape = true;
+        //source_blob.FromProto(source_layer.blobs(j), kReshape);
+        //LOG(FATAL) << "Cannot copy param " << j << " weights from layer '"
+        //    << source_layer_name << "'; shape mismatch.  Source param shape is "
+        //    << source_blob.shape_string() << "; target param shape is "
+        //    << target_blobs[j]->shape_string() << ". "
+        //    << "To learn this layer's parameters from scratch rather than "
+        //    << "copying from a saved net, rename the layer.";
       }
-      const bool kReshape = false;
-      target_blobs[j]->FromProto(source_layer.blobs(j), kReshape);
+      else {
+        const bool kReshape = false;
+        target_blobs[j]->FromProto(source_layer.blobs(j), kReshape);
+      }
     }
   }
 }
