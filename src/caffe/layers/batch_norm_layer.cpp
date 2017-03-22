430522cd4d848f75e09f782236103e7b076fb4eb
happynear <wf900911@163.com>
2017-03-21 20:14:28 -0400

    don't check lr when engine=cudnn

--- src/caffe/layers/batch_norm_layer.cpp
+++ src/caffe/layers/batch_norm_layer.cpp
@@ -36,16 +36,19 @@ void BatchNormLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
                 this->blobs_[i]->mutable_cpu_data());
     }
   }
-  // Mask statistics from optimization by setting local learning rates
-  // for mean, variance, and the bias correction to zero.
-  for (int i = 0; i < this->blobs_.size(); ++i) {
-    if (this->layer_param_.param_size() == i) {
-      ParamSpec* fixed_param_spec = this->layer_param_.add_param();
-      fixed_param_spec->set_lr_mult(0.f);
-    } else {
-      CHECK_EQ(this->layer_param_.param(i).lr_mult(), 0.f)
+  if (param.engine() != BatchNormParameter_Engine_CUDNN) {
+    // Mask statistics from optimization by setting local learning rates
+    // for mean, variance, and the bias correction to zero.
+    for (int i = 0; i < this->blobs_.size(); ++i) {
+      if (this->layer_param_.param_size() == i) {
+        ParamSpec* fixed_param_spec = this->layer_param_.add_param();
+        fixed_param_spec->set_lr_mult(0.f);
+      }
+      else {
+        CHECK_EQ(this->layer_param_.param(i).lr_mult(), 0.f)
           << "Cannot configure batch normalization statistics as layer "
           << "parameters.";
+      }
     }
   }
 }
