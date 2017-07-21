942e1863a7a3ff64cb412165a3465a5909286212
happynear <wf900911@163.com>
2017-07-21 10:29:17 -0400

    remove bn_layer definition.

--- include/caffe/custom_layers.hpp
+++ include/caffe/custom_layers.hpp
@@ -14,61 +14,6 @@
 
 namespace caffe {
 	/**
-	* @brief Takes at least two Blob%s and concatenates them along either the num
-	*        or channel dimension, outputting the result.
-	*/
-	template <typename Dtype>
-	class BNLayer : public Layer<Dtype> {
-	public:
-		explicit BNLayer(const LayerParameter& param)
-			: Layer<Dtype>(param) {}
-		virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-			const vector<Blob<Dtype>*>& top);
-		virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-			const vector<Blob<Dtype>*>& top);
-
-		virtual inline const char* type() const { return "BN"; }
-		virtual inline int ExactNumBottomBlobs() const { return 1; }
-		virtual inline int ExactNumTopBlobs() const { return 1; }
-
-	protected:
-		virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-			const vector<Blob<Dtype>*>& top);
-		virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-			const vector<Blob<Dtype>*>& top);
-		virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-			const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-		virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-			const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-
-		// spatial mean & variance
-		Blob<Dtype> spatial_statistic_;
-		// batch mean & variance
-		Blob<Dtype> batch_statistic_;
-		// buffer blob
-		Blob<Dtype> buffer_blob_;
-		// x_norm and x_std
-		Blob<Dtype> x_norm_, x_std_;
-		// Due to buffer_blob_ and x_norm, this implementation is memory-consuming
-		// May use for-loop instead
-
-		// x_sum_multiplier is used to carry out sum using BLAS
-		Blob<Dtype> spatial_sum_multiplier_, batch_sum_multiplier_;
-
-		// dimension
-		int num_;
-		int channels_;
-		int height_;
-		int width_;
-		// eps
-		Dtype var_eps_;
-		// decay factor
-		Dtype decay_;
-		// whether or not using moving average for inference
-		bool moving_average_;
-
-	};
-	/**
 	* @brief Randomized Leaky Rectified Linear Unit @f$
 	*        y_i = \max(0, x_i) + frac{\min(0, x_i)}{a_i}
 	*        @f$. The negative slope is randomly generated from
